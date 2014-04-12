1896171407202860918f338348ee7db8ef9b2f31
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2014-04-11 23:29:31 -0700

    Hook into @ivar=
    --HG--
    extra : amend_source : 0d886d351a4db3ae668fee0ec9e4c3dc70b7ecf8

--- sandbox.rb
+++ sandbox.rb
@@ -3,4 +3,35 @@ require_relative 'myco/toolset'
 require_relative 'myco/compiler'
 require_relative 'myco/eval'
 
-p Myco.eval "99"
+module Myco::ToolSet
+  module AST
+    class InstanceVariableAssignment
+      def bytecode(g)
+        @value.bytecode(g) if @value
+
+        pos(g)
+        
+        # Call Myco.updated object, name, value
+        g.dup_top
+        g.push_const :Myco
+        g.rotate 2
+        g.push_literal @name
+        g.push_self
+        g.rotate 3
+        g.send :updated, 3, false
+        g.pop
+        # Resume normal operation
+        
+        g.set_ivar @name
+      end
+    end
+  end
+end
+
+class Myco
+  def self.updated object, name, value
+    puts "#{object} #{name.inspect} = #{value.inspect}"
+  end
+end
+
+Myco.eval "Object.new.instance_eval { @thing = 88; @thing = 99 }"
