96d8235a5f601f62e9b7b1f36cc4b9c2a7a39ea6
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2014-04-16 23:12:26 -0700

    More tests in lexer_spec
    --HG--
    extra : amend_source : 3ff5e9cf9266f31bbd497435424882db8c596c8d

--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -17,14 +17,116 @@ describe Myco::ToolSet::Parser::Lexer do
   end
   
   
-  lex "Object"  do  [[:T_CONSTANT, "Object", 1]]   end
-  lex "OBJECT"  do  [[:T_CONSTANT, "OBJECT", 1]]   end
-  lex "Obj_3cT" do  [[:T_CONSTANT, "Obj_3cT", 1]]  end
+  describe "Constants" do
+    
+    lex "Object"  do  [[:T_CONSTANT, "Object", 1]]   end
+    lex "OBJECT"  do  [[:T_CONSTANT, "OBJECT", 1]]   end
+    lex "Obj_3cT" do  [[:T_CONSTANT, "Obj_3cT", 1]]  end
+    
+  end
   
-  lex "Object { }" do 
-    [[:T_CONSTANT, "Object", 1],
-     [:T_DECLARE_BEGIN, "{", 1],
-     [:T_DECLARE_END, "}", 1]]
+  
+  describe "Declarations" do
+    
+    lex "Object { }" do
+      [[:T_CONSTANT, "Object", 1],
+       [:T_DECLARE_BEGIN, "{", 1],
+       [:T_DECLARE_END, "}", 1]]
+    end
+    
+    lex "Object{}" do
+      [[:T_CONSTANT, "Object", 1],
+       [:T_DECLARE_BEGIN, "{", 1],
+       [:T_DECLARE_END, "}", 1]]
+    end
+    
+    lex <<-code do
+      Object {
+        
+      }
+    code
+      [[:T_CONSTANT, "Object", 1],
+       [:T_DECLARE_BEGIN, "{", 1],
+       [:T_DECLARE_END, "}", 3]]
+    end
+    
+    lex <<-code do
+      Object
+      {
+        
+      }
+    code
+      [[:T_CONSTANT, "Object", 1],
+       [:T_DECLARE_BEGIN, "{", 2],
+       [:T_DECLARE_END, "}", 4]]
+    end
+    
+  end
+  
+  
+  describe "Bindings" do
+    
+    lex <<-code do
+      Object {
+        foo:    one
+        bar  :  two  
+        baz    :three
+        all:zero
+      }
+    code
+      [[:T_CONSTANT, "Object", 1], [:T_DECLARE_BEGIN, "{", 1],
+       [:T_IDENTIFIER, "foo", 2],  [:T_BINDING_BEGIN, "", 2],
+         [:T_IDENTIFIER, "one", 2],  [:T_BINDING_END, "", 3],
+       [:T_IDENTIFIER, "bar", 3],  [:T_BINDING_BEGIN, "", 3],
+         [:T_IDENTIFIER, "two", 3],  [:T_BINDING_END, "", 4],
+       [:T_IDENTIFIER, "baz", 4],  [:T_BINDING_BEGIN, "", 4],
+         [:T_IDENTIFIER, "three", 4],[:T_BINDING_END, "", 5],
+       [:T_IDENTIFIER, "all", 5],  [:T_BINDING_BEGIN, "", 5],
+         [:T_IDENTIFIER, "zero", 5], [:T_BINDING_END, "", 6],
+       [:T_DECLARE_END, "}", 6]]
+    end
+    
+    lex <<-code do
+      Object {
+        foo:  {  one  }
+        bar  :{  two  }
+        baz    :{three}  
+        all:{zero }
+      }
+    code
+      [[:T_CONSTANT, "Object", 1], [:T_DECLARE_BEGIN, "{", 1],
+       [:T_IDENTIFIER, "foo", 2],  [:T_BINDING_BEGIN, "{", 2],
+         [:T_IDENTIFIER, "one", 2],  [:T_BINDING_END, "}", 2],
+       [:T_IDENTIFIER, "bar", 3],  [:T_BINDING_BEGIN, "{", 3],
+         [:T_IDENTIFIER, "two", 3],  [:T_BINDING_END, "}", 3],
+       [:T_IDENTIFIER, "baz", 4],  [:T_BINDING_BEGIN, "{", 4],
+         [:T_IDENTIFIER, "three", 4],[:T_BINDING_END, "}", 4],
+       [:T_IDENTIFIER, "all", 5],  [:T_BINDING_BEGIN, "{", 5],
+         [:T_IDENTIFIER, "zero", 5], [:T_BINDING_END, "}", 5],
+       [:T_DECLARE_END, "}", 6]]
+    end
+    
+    lex <<-code do
+      Object {
+        foo: {
+          one
+        }
+        bar:{ two
+          }
+        baz  :{
+          three}
+      }
+    code
+      [[:T_CONSTANT, "Object", 1], [:T_DECLARE_BEGIN, "{", 1],
+      [:T_IDENTIFIER, "foo", 2],   [:T_BINDING_BEGIN, "{", 2],
+        [:T_IDENTIFIER, "one", 3],   [:T_BINDING_END, "}", 4],
+      [:T_IDENTIFIER, "bar", 5],   [:T_BINDING_BEGIN, "{", 5],
+        [:T_IDENTIFIER, "two", 5],   [:T_BINDING_END, "}", 6],
+      [:T_IDENTIFIER, "baz", 7],   [:T_BINDING_BEGIN, "{", 7],
+        [:T_IDENTIFIER, "three", 8], [:T_BINDING_END, "}", 8],
+      [:T_DECLARE_END, "}", 9]]
+    end
+    
   end
   
 end
