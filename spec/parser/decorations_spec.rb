a4cc09e5a4d58e6b068636a0027787a97024751b
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2014-04-29 19:36:57 -0700

    Introduce a few files to version control that should have been added earlier

---
+++ spec/parser/decorations_spec.rb
@@ -0,0 +1,28 @@
+
+require 'spec_helper'
+
+
+describe Myco::ToolSet::Parser, "Decorations" do
+  extend SpecHelpers::ParserHelper
+  
+  lex <<-code do
+    Object {
+      on foo: nil
+      one two three: nil
+    }
+  code
+    [[:T_CONSTANT, "Object"], [:T_DECLARE_BEGIN, "{"],
+     [:T_IDENTIFIER, "on"], [:T_IDENTIFIER, "foo"],
+       [:T_BINDING_BEGIN, ""], [:T_NIL, "nil"], [:T_BINDING_END, ""],
+     [:T_IDENTIFIER, "one"], [:T_IDENTIFIER, "two"], [:T_IDENTIFIER, "three"],
+       [:T_BINDING_BEGIN, ""], [:T_NIL, "nil"], [:T_BINDING_END, ""],
+     [:T_DECLARE_END, "}"]]
+  end
+  .parse [:declobj, [:array, [:const, :Object]], [:block,
+    [:bind, :foo,   [:array, [:lit, :on]], 
+      [:args], [:block, [:nil]]],
+    [:bind, :three, [:array, [:lit, :two], [:lit, :one]], 
+      [:args], [:block, [:nil]]]
+  ]]
+  
+end
