14a0810ce300049d6249d4a7c5b5ecd8a07d8758
Joe McIlvain <joe.eli.mac@gmail.com>
2014-08-20 21:26:51 -0700

    Added parser specs for jump nodes

---
+++ spec/parser/jumps_spec.rb
@@ -0,0 +1,24 @@
+
+require 'spec_helper'
+
+
+describe Myco::ToolSet::Parser, "Jumps" do
+  extend SpecHelpers::ParserHelper
+  
+  parse <<-'code' do
+    Object {
+      foo: {
+        77
+        88 ->
+        99
+      }
+    }
+  code
+    [:declobj, [:array, [:const, :Object]], [:block,
+      [:meme, :foo, [:array], [:args], [:block,
+        [:lit, 77], [:return, [:lit, 88]], [:lit, 99]
+      ]]
+    ]]
+  end
+  
+end
