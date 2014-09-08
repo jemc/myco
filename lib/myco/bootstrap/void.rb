fe3105622299553e22a1b54ba45e514f108872dc
Joe McIlvain <joe.eli.mac@gmail.com>
2014-09-08 00:45:34 -0700

    Added void.void?

--- lib/myco/bootstrap/void.rb
+++ lib/myco/bootstrap/void.rb
@@ -17,6 +17,10 @@ module Myco
       true
     end
     
+    def void?
+      true
+    end
+    
     def method_missing *args
       self
     end
@@ -30,7 +34,7 @@ module Myco
 end
 
 # Patch the base classes to make them respond_to :false?
-class ::NilClass;    def false?; true  end end
-class ::TrueClass;   def false?; false end end
-class ::FalseClass;  def false?; true  end end
-class ::BasicObject; def false?; false end end
+class ::NilClass;    def false?; true  end; def void?; false end end
+class ::TrueClass;   def false?; false end; def void?; false end end
+class ::FalseClass;  def false?; true  end; def void?; false end end
+class ::BasicObject; def false?; false end; def void?; false end end
