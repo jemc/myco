d4fed619e8bc08c7c31ba5284e7048ae5319eb7f
Joe McIlvain <joe.eli.mac@gmail.com>
2014-09-11 19:03:40 -0700

    Use bytecode reimplementation of result_for in Meme dynamic_method

--- lib/myco/bootstrap/meme.rb
+++ lib/myco/bootstrap/meme.rb
@@ -7,6 +7,8 @@ module Myco
     attr_accessor :cache
     attr_accessor :expose
     
+    attr_reader :caches # TODO: don't expose; dynamic meth should send privately
+    
     def to_s
       "#<#{self.class}:#{self.name.to_s}>"
     end
@@ -51,20 +53,86 @@ module Myco
       target.memes[@name] = meme
       
       ##
-      # Make the forwarding method as streamlined as possible
-      # by defining with bytecode instead of using define_method
+      # This dynamic method is nearly the same as Meme#result_for
+      # (but written from the perspective of the receiver)
+      # implemented in bytecode to avoid forwarding to another method
+      # on the call stack.
       # TODO: move this bytecode generation to a helper method 
-      target.dynamic_method @name do |g|
+      target.dynamic_method @name, '(myco_internal)' do |g|
         g.splat_index = 0 # *args
         
-        ##
-        # meme.result_for(self, *args, &block)
+        invoke = g.new_label
+        ret    = g.new_label
         
+        ##
+        # meme = <this meme>
+        #
         g.push_literal meme
+        g.set_local 1 # meme
+        g.pop
+        
+        ##
+        # caches = <this meme's @caches>
+        #
+        g.push_literal @caches
+        g.set_local 2 # caches
+        g.pop
+        
+        ##
+        # cache_key = [obj.hash, args.hash, blk.hash]
+        #
+          g.push_self;     g.send :hash, 0
+          g.push_local 0;  g.send :hash, 0 # args
+          g.push_block;    g.send :hash, 0
+        g.make_array 3
+        g.set_local 3 # cache_key
+        g.pop
+        
+        ##
+        # if meme.cache.false? || !caches.has_key?(cache_key)
+        #   return caches[cache_key]
+        # end
+        #
+        g.push_local 1 # meme
+        g.send :cache, 0
+        g.send :false?, 0
+        g.goto_if_true invoke
+        
+        g.push_local 2 # caches
+          g.push_local 3 # cache_key
+        g.send :has_key?, 1
+        g.goto_if_false invoke
+        
+        g.push_local 2 # caches
+          g.push_local 3 # cache_key
+        g.send :[], 1
+        g.goto ret
+        
+        ##
+        # result = meme.body.invoke meme.name, @target, obj, args, blk
+        #
+        invoke.set!
+        
+        g.push_local 1 # meme
+        g.send :body, 0
+          g.push_local 1; g.send :name, 0   # meme.name
+          g.push_local 1; g.send :target, 0 # meme.target
           g.push_self
-          g.push_local 0 # *args
+          g.push_local 0 # args
           g.push_block
-        g.send_with_splat :result_for, 1
+        g.send :invoke, 5
+        g.set_local 4 # result
+        g.pop
+        
+        ##
+        # return (caches[cache_key] = result)
+        #
+        g.push_local 2 # caches
+          g.push_local 3 # cache_key
+          g.push_local 4 # result
+        g.send :[]=, 2
+        
+        ret.set!
         g.ret
       end
     end
