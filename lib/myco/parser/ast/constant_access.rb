6b8a478ab1bb64c17d6a3247bcdc6a1cf82b1b8f
Joe McIlvain <joe.eli.mac@gmail.com>
2014-09-25 18:42:12 -0700

    Remove unnecessary branch in AST::ConstantAccess#bytecode

--- lib/myco/parser/ast/constant_access.rb
+++ lib/myco/parser/ast/constant_access.rb
@@ -6,23 +6,15 @@ module CodeTools::AST
     def bytecode(g)
       pos(g)
       
-      if g.state.op_asgn? # TODO: is this branch unnecessary with Myco AST?
+      if @top_level
+        g.push_cpath_top
+        g.find_const @name
+      else
         g.push_cpath_top
         g.find_const :Myco
         g.push_literal @name
         g.push_scope
-        g.send :find_constant_for_op_asign_or, 2
-      else
-        if @top_level
-          g.push_cpath_top
-          g.find_const @name
-        else
-          g.push_cpath_top
-          g.find_const :Myco
-          g.push_literal @name
-          g.push_scope
-          g.send :find_constant, 2
-        end
+        g.send :find_constant, 2
       end
     end
   end
