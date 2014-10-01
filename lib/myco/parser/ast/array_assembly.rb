6ae3c0b0a7869bc81715c3e514d3dc0981e7d6af
Joe McIlvain <joe.eli.mac@gmail.com>
2014-09-30 21:15:54 -0700

    Make bytecode for AST::ArrayAssembly less wasteful
    --HG--
    extra : amend_source : 8a37459de6bad7e8bbab7028fd03e85a00584128

--- lib/myco/parser/ast/array_assembly.rb
+++ lib/myco/parser/ast/array_assembly.rb
@@ -20,17 +20,37 @@ module CodeTools::AST
       
       if @body.empty?
         g.make_array(0)
-      else
-        @body.each_with_index do |item, idx|
-          item.bytecode(g)
-          g.make_array(1) unless item.is_a?(SplatValue)
-          g.send(:+, 1) unless idx==0
+        return
+      end
+      
+      # Group the @body into chunks of splats and non-splats
+      chunked = @body.chunk { |item| item.is_a?(SplatValue) }
+      
+      # Each SplatValue outputs the bytecode of a single array
+      # Non-SplatValues are grouped to output the bytecode of
+      # a single array for each contiguous group.  Along the way, the
+      # arrays are concatenated to form one final array on the stack.
+      first_bytecode = true
+      chunked.each do |is_splat_group, group|
+        if is_splat_group
+          group.each { |item|
+            item.bytecode(g)
+            g.send(:concat, 1) unless first_bytecode
+            first_bytecode = false
+          }
+        else
+          group.each { |item|
+            item.bytecode(g)
+          }
+          g.make_array(group.size)
+          g.send(:concat, 1) unless first_bytecode
+          first_bytecode = false
         end
       end
     end
     
     def to_sexp
-      @body.inject([:array]) { |sexp, item| sexp.push(item.to_sexp) }
+      [:array] + @body.map(&:to_sexp)
     end
   end
    
