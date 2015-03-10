84634530da7c7921e522bd5495e6cd04d49eb347
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-03-09 22:37:44 -0700

    Refactor bootstrap phases of lib/myco.rb

--- lib/myco.rb
+++ lib/myco.rb
@@ -8,25 +8,18 @@ require_relative 'myco/misc'
 require_relative 'myco/backtrace'
 require_relative 'myco/bootstrap'
 
-Myco.rescue do
-  # TODO: be more clever here communicating the load path for bootstrapping
-  Myco::CoreLoadPath = File.expand_path('myco', File.dirname(__FILE__))
-  Myco.eval_file 'myco/bootstrap.my'
-end
-
-# Load the boot toolset from (possibly old) cached bytecode files
-module Myco
-  ToolSet = Rubinius::ToolSets.create :myco_boot do
-    require "rubinius/melbourne"
-    require "rubinius/compiler"
+# First pass:  Load the boot toolset
+# Second pass: Load the runtime toolset using the boot toolset to parse it
+# TODO: detect whether a second pass is really necessary (ie, booting from ruby)
+[:myco_boot, :myco].each do
+  Myco::ToolSet = Rubinius::ToolSets.create :myco_boot do
     
-    require_relative 'myco/parser'
-  end
-end
-
-# Load the runtime toolset from the latest code
-module Myco
-  ToolSet = Rubinius::ToolSets.create :myco do
+    Myco.rescue do
+      # TODO: be more clever here communicating the load path for bootstrapping
+      Myco::CoreLoadPath = File.expand_path('myco', File.dirname(__FILE__))
+      Myco.eval_file 'myco/bootstrap.my'
+    end
+    
     require "rubinius/melbourne"
     require "rubinius/compiler"
     
