092df3e4776ac960bfe77e4d194c374c7d77d59d
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-04-29 00:57:34 -0700

    Add Myco.undefined method.

--- lib/myco/bootstrap.rb
+++ lib/myco/bootstrap.rb
@@ -2,6 +2,7 @@
 require_relative 'bootstrap/find_constant'
 
 require_relative 'bootstrap/void'
+require_relative 'bootstrap/undefined'
 require_relative 'bootstrap/tuple'
 require_relative 'bootstrap/meme'
 require_relative 'bootstrap/instance'
