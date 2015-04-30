6b1243a8bdc469ecababc6b6262cd8f7b73b7118
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-04-29 23:38:55 -0700

    Add Myco::PrimitiveTuple as the base for Myco::Instance.

--- lib/myco/bootstrap/instance.rb
+++ lib/myco/bootstrap/instance.rb
@@ -1,4 +1,22 @@
 
+# This is a workaround because Rubinius doesn't expose Class::set_type_info(),
+# so we cannot simply assign the type_info for our new class to TupleType.
+# So, we dup a class for its type_info, then clear all methods and ancestors.
+Myco::PrimitiveTuple = Rubinius::Tuple.dup
+Myco::PrimitiveTuple.instance_eval do
+  set_superclass(nil)
+  
+  instance_methods.each do |name|
+    method_table.delete(name)
+    Rubinius::VM.reset_method_cache(self, name)
+  end
+  
+  methods(false).each do |name|
+    singleton_class.method_table.delete(name)
+    Rubinius::VM.reset_method_cache(singleton_class, name)
+  end
+end
+
 module Myco
   module PrimitiveInstanceMethods
     # These methods are taken from Ruby's Kernel.
@@ -84,8 +102,21 @@ module Myco
     end
   end
   
-  Instance = Class.new nil do
+  Instance = Class.new Myco::PrimitiveTuple do
     include InstanceMethods
+    
+    def self.__tuple_size__
+      0
+    end
+    
+    def self.__tuple_allocate__ size
+      Rubinius.primitive :tuple_allocate
+      ::Kernel.raise ::PrimitiveFailure, "Rubinius.primitive :tuple_allocate failed"
+    end
+    
+    def self.allocate
+      __tuple_allocate__(__tuple_size__)
+    end
     
     # These are not included in InstanceMethods because they should not shadow
     # existing method definitions when extended into an existing object. 
